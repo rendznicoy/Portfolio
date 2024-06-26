e0fe5ec05aac7a6bca33a690ff7f3d0c107469ed
Rene Angelo de los Reyes <21-1-01027@vsu.edu.ph>
2024-06-27 07:58:40 +0800

    Update
    * Fixed a scroll transition problem

--- script.js
+++ script.js
@@ -33,17 +33,19 @@ document.addEventListener("DOMContentLoaded", () => {
     navMenu.classList.toggle("active");
     navToggle.classList.toggle("active");
   });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("scrollButton1")
-    .addEventListener("click", (event) => {
+    .addEventListener("click", function (event) {
       event.preventDefault();
       document.getElementById("about").scrollIntoView({ behavior: "smooth" });
     });
 
   document
     .getElementById("scrollButton2")
-    .addEventListener("click", (event) => {
+    .addEventListener("click", function (event) {
       event.preventDefault();
       document
         .getElementById("projects")
@@ -52,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document
     .getElementById("scrollButton3")
-    .addEventListener("click", (event) => {
+    .addEventListener("click", function (event) {
       event.preventDefault();
       document
         .getElementById("experience")
@@ -61,22 +63,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document
     .getElementById("scrollButton4")
-    .addEventListener("click", (event) => {
+    .addEventListener("click", function (event) {
       event.preventDefault();
       document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
     });
 
   document
     .getElementById("scrollButton5")
-    .addEventListener("click", (event) => {
+    .addEventListener("click", function (event) {
       event.preventDefault();
       document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
     });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
   const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
 
   smoothScrollLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
       const targetId = this.getAttribute("href").substring(1);
       const targetSection = document.getElementById(targetId);
